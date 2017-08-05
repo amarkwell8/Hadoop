90d51e4d879416bd8a054890a3237e06f19cc07e
Austin Markwell <amarkwell8@gmail.com>
2017-08-05 14:49:45 -0700

    Uploaded basic word counter

---
+++ WordCount/src/counter/Reduce.java
@@ -0,0 +1,20 @@
+package counter;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+
+public class Reduce extends Reducer<Text, IntWritable, Text, LongWritable>{
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
+        long count = 0;
+        for(IntWritable value:values){
+            count++;
+        }
+        context.write(key, new LongWritable(count));
+    }
+}
